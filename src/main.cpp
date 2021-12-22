60cc9cf872fb837127039f6ec2d5d6bfea7a0bdb
edroque93 <quique.lpgc@gmail.com>
2021-12-22 10:53:11 +0000

    Hello world

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <hidapi.h>
+
+int main(const int argc, const char **argv)
+{
+	std::cout << "Hello world" << std::endl;
+	int resinit = hid_init();
+	int resexit = hid_exit();
+
+	return 0;
+}
